78c6d4787d55ba1e00e0276be26c7e9ca37aa0bf
PierrickHOUEE <houee.pierrick@gmail.com>
2022-11-29 19:13:00 +0100

    rejected by craigslist

--- getter.ts
+++ getter.ts
@@ -1,7 +1,23 @@
 import request from "request-promise";
+import fs from "fs";
+// Trying with a proxy
+const usedRequest = request.defaults({
+  proxy: "20.111.54.16:80",
+});
 
-async function getHtml(url) {
+async function getHtml(url: string) {
   const html = await request.get(url);
 }
 
-function saveHtmlToFile(html) {}
+function saveHtmlToFile(html: any) {
+  fs.writeFileSync("./test.html", html);
+}
+
+async function main() {
+  const html: any = await getHtml(
+    "https://sfbay.craigslist.org/search/muc#search=1~list~0~0"
+  );
+  html && saveHtmlToFile(html);
+}
+
+main();
